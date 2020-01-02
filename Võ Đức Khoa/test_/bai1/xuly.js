d9edb347cf701e1c78f7dd23a57385720d4411df
vokhoa1998 <58552724+vokhoa1998@users.noreply.github.com>
2020-01-02 20:41:07 +0700

    sua

--- Võ Đức Khoa/test_/bai1/xuly.js
+++ Võ Đức Khoa/test_/bai1/xuly.js
@@ -1,40 +1,32 @@
 var $ = function(id){
-    return document.getElementById('id');
+    return document.getElementById(id);
 }
-var validate = function(){
-    var sale = $('sale').value;
-    return (sale.trim() == '' || sale < 0 || isNaN(sale)) ? false : true;
+var validate = function(value){
+    return (value.trim() == '' || value < 0 || isNaN(value)) ? false : true;
 }
 $('submit').onclick = function(){
     var sale =parseFloat( $('sale').value).toFixed(2);
-    var salary = $('salary').value;
-    var tax = $('tax').value;
+    var salary;
+    var tax;
     var check = validate(sale);
     if(check){
         if(sale < 5000){
-            salary = sale*(5/100);
-            tax = salary > 1000 ? 10 + '%' : 0+ '%';
+            salary = sale*0.05;          
         }
-        else if(sale > 5000 && sale <10000){
-            salary = sale*(10/100);
-            tax = salary > 1000 ? 10 + '%' : 0+ '%';
+        else if(sale >= 5000 && sale <=10000){
+            salary = sale*0.1
         }
         else{
-            salary = sale*(15/100);
-            tax = salary > 1000 ? 10 + '%' : 0+ '%';
+            salary = sale*0.15        
         }
+        tax = salary > 1000 ? 10 : 0;
     }
     else alert('Error');
     if(check){
-        var salary =parseFloat( $('salary').value).toFixed(2);
-        var tax = $('tax').value;
-        var total = salary - salary*tax;
+        $('salary').value = parseFloat(salary).toFixed(2);
+        $('tax').value = tax + '%';
+        var total = salary - salary*(tax/100);
         $('total').value  = parseFloat(total).toFixed(2);
     }
     else alert('Error');
-}
-$('reset').onclick = function(){
-     $('sale').value = '';
-     $('salary').value = '';
-     $('tax').value = '';
 }
