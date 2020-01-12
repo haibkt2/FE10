22994055a5439a1b64d35b1c2572ccbc2ee13264
Huy Anh <nguyenquanghuy3797@gmail.com>
2020-01-12 15:46:24 +0700

    bt 2

--- Quang Huy/jQuery/jQuery_Day2/Js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("div").toggle();
-    $("span").toggleClass("glyphicon glyphicon-plus");
-    $("h2").click(function() {
-        $(this).next().toggle();
-        $(this).children("span").toggleClass("glyphicon glyphicon-minus");
-        $(this).children("a").toggleClass("rs");
-    });
-});
