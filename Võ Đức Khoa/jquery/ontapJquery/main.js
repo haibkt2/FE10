7e1b9241de35b1ce55b9fd564e3bbdd27c2beaca
vokhoa1998 <58552724+vokhoa1998@users.noreply.github.com>
2020-04-21 19:37:20 +0700

    chuc nang save

--- Võ Đức Khoa/jquery/ontapJquery/main.js
+++ Võ Đức Khoa/jquery/ontapJquery/main.js
@@ -1,1 +1,38 @@
-// chuc nang cua bottom add
+// chuc nang cua bottom save and show
+var users = [];
+$(".save").click(function(){
+    var name = $("#name").val();
+    var address = $("#address").val();
+    var password = $("#password").val();
+    var code = $("#code").val();
+    var email = $("#email").val();
+    var username = $("#username").val();
+    const user = {
+        id : 0,
+        name : name,
+        address : address,
+        password : password,
+        code : code,
+        email : email,
+        username : username
+    }
+    users.push(user);
+    showUser();
+   
+})
+function showUser(){
+    users.forEach(item => {
+        $("#tbody").append(
+            `
+                <tr>
+                    <th style="width:45px"></th>
+                    <th>${item.code}</th>
+                    <th>${item.name}</th>
+                    <th>${item.address}</th>
+                    <th>${item.email}</th>
+                    <th>${item.username}</th>
+                </tr>
+            `
+        )
+    })
+}
