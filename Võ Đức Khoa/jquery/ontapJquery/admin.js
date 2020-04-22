f501f7c9ec62933eebff7304dac509756c9df541
vokhoa1998 <58552724+vokhoa1998@users.noreply.github.com>
2020-04-22 18:03:01 +0700

    ontap-jquery

---
+++ Võ Đức Khoa/jquery/ontapJquery/admin.js
@@ -0,0 +1,111 @@
+//setdefault admin
+function setAdmin(){  
+    localStorage.setItem("admin","admin")
+}
+setAdmin();
+ // validate
+//  $("#form_sign").validate({
+// 	rules : {
+// 	    code :{
+//             required : true
+//         },
+//         username :{
+//             required : true
+//         },
+// 		email : {
+// 		   required : true,
+// 		   email : true
+		   
+// 		},
+// 		name : {
+//             required : true
+// 		},
+// 		pass : {
+// 			required : true,
+// 			minlength : 6
+// 		},
+// 		re_pass : {
+// 			equalTo : "#pass"
+//         },
+//         address :{
+//             required: true
+//         }	
+// 	}, 
+// 	messages : {
+//         code :{
+//           required: "Enter code"
+//         },
+//         username :{
+//             required : "Enter username"
+//         },
+// 		email : {
+// 		  required : "Enter email",
+// 		  email : "nhap theo dung kieu email"
+// 		},
+// 		name : {
+//            required : "enter name"
+// 		},
+// 		pass : {
+// 			required : "input pass",
+// 			minlength : jQuery.validator.format("Min is {0}")
+// 		},
+// 		re_pass : {
+// 			equalTo : "Not same"
+//         },
+//         address :{
+//             required : "Enter address"
+//         }
+// 	}
+// })
+// xu ly login
+// tao constructor
+var user = function(name,address,password,code,email,username){
+	   this.name = name;
+       this.address = address;
+       this.password = password;
+       this.code = code;
+       this.email = email;
+       this.username = username;
+
+}
+var arr = [];
+$("#sign_up").click(function(){
+		var name = $("#name").val(), 
+		 mail = $("#email").val(), 
+		 pass = $("#pass").val(),
+         username = $("#username").val(),
+		code = $("#code").val(),
+		address = $("#address").val();
+        var user_if4 = new user(name,address,pass,code,mail,username);
+        arr.push(user_if4);
+		var user_json = JSON.stringify(arr);
+        localStorage.setItem("usered", user_json);
+        $("#form_sign").attr("action","login.html")
+        $("#form_sign").submit();
+});
+$("#login").click(function(){
+	var name = $("#name_login").val();
+	var pass = $("#pass_login").val();
+    var key = localStorage.getItem("usered");
+    var admin = localStorage.getItem("admin");
+    if(admin == pass){
+        if(name = "admin"){
+            $("#form_login").attr("action","manager.html");
+            $("#form_login").submit();
+        }
+    }
+    else
+	   if( key == null) alert("tai khoan nay chua duoc tao hay dang register account");
+       else{
+        var usered = JSON.parse(key);
+        usered.forEach(item =>{
+         if( item.password == pass) {
+             $("#form_login").attr("action","https://www.facebook.com/khoa.voduckhoa");
+             $("#form_login").submit();
+             }
+         else alert('sai password');
+        })              
+     }		
+        
+          
+});
--- Võ Đức Khoa/jquery/ontapJquery/main.js
+++ Võ Đức Khoa/jquery/ontapJquery/main.js
@@ -1,38 +1,134 @@
-// chuc nang cua bottom save and show
-var users = [];
-$(".save").click(function(){
-    var name = $("#name").val();
-    var address = $("#address").val();
-    var password = $("#password").val();
-    var code = $("#code").val();
-    var email = $("#email").val();
-    var username = $("#username").val();
-    const user = {
-        id : 0,
-        name : name,
-        address : address,
-        password : password,
-        code : code,
-        email : email,
-        username : username
+
+var users ;
+function getUser(){
+   var usered = localStorage.getItem("usered");
+   users = (usered == null) ? [] : JSON.parse(usered);
+}
+getUser();
+showUser();
+// chuc nang cua button add
+$(".add").click(function(){
+    if( $("#code").val().trim() != "" ){
+   //get value form tag input
+   var name = $("#name").val();
+   var address = $("#address").val();
+   var password = $("#password").val();
+   var code = $("#code").val();
+   var email = $("#email").val();
+   var username = $("#username").val();
+   // luu vao mot object
+   const user = {
+       name : name,
+       address : address,
+       password : password,
+       code : code,
+       email : email,
+       username : username
+   }
+   //truyen vao array
+   users.push(user);
+   //reset form input
+  $("#form").trigger("reset");   
+   showUser();
+   alert("save de luu data");
     }
-    users.push(user);
-    showUser();
+    else alert('bat buoc nhap code');  
    
 })
+// chuc nang show
 function showUser(){
+    $("#tbody").text('');
+    $('#tbody').append(`
+    <tr style="background-color: #39d38f">
+                    <th style="width:45px"></th>
+                    <th>Code</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Email</th>
+                    <th>UserName</th>
+                </tr>
+    `)
     users.forEach(item => {
         $("#tbody").append(
             `
                 <tr>
-                    <th style="width:45px"></th>
-                    <th>${item.code}</th>
-                    <th>${item.name}</th>
-                    <th>${item.address}</th>
-                    <th>${item.email}</th>
-                    <th>${item.username}</th>
+                    <td style="width:45px"><input type = "radio" id = "select" name="select"></td>
+                    <td>${item.code}</td>
+                    <td id ="itemName">${item.name}</td>
+                    <td>${item.address}</td>
+                    <td>${item.email}</td>
+                    <td>${item.username}</td>
                 </tr>
             `
         )
     })
 }
+// chuc nang cua button delete
+//tao bien trung gian giua checkbox va delete button
+var temp = "";
+var check = false;
+$("#tbody").on("click","input",function(){
+    check = true;
+    var checkbox = $(this);
+    temp = checkbox.parent().next().text();       
+})
+//event xoa object
+var filter = [];
+$(".delete").click(function(){
+   if(check == true){
+         filter = users.filter(item => {
+            return item.code != temp;
+        })
+        users = [...filter];
+        showUser();
+        check = false;
+        alert("save de luu data");
+    }
+   else alert("hay chon user")
+})  
+//chuc nang edit
+$(".edit").click(function(){
+   if(check == true){
+    users.forEach(item => {
+        if(item.code == temp){
+            $("#name").val(`${item.name}`)
+            $("#code").val(`${item.code}`)
+            $("#email").val(`${item.email}`)
+            $("#address").val(`${item.address}`)
+            $("#password").val(`${item.password}`)
+            $("#username").val(`${item.username}`)
+        }
+    })
+    check == false;
+    alert("save de luu data");
+   }
+   else alert("hay chon user");
+})
+// chuc nang luu cua button save
+$(".save").click(function(){
+ //  if( $("#code").val().trim() != ""){
+        users.forEach(item => {
+            if(item.code == temp){
+                item.code = $("#code").val();
+                item.name = $("#name").val();
+                item.address = $("#address").val();
+                item.password = $("#password").val();
+                item.email = $("#email").val();
+                item.username = $("#username").val();      
+                
+            }
+       })
+       $("#form").trigger("reset");  
+       showUser(); 
+       var listUser = JSON.stringify(users);
+       localStorage.setItem("usered",listUser);
+       console.log(users);
+//}
+ //  else alert("Hay edit truoc roi save");
+   })
+
+//chuc nang exit save current
+$(".exit").click(function(){
+    $("#form").trigger("reset");  
+})
+
